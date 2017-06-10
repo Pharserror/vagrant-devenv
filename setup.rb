83739e9cbef64172b9c69d34b3919bb8e0479b4e
Adam Lind <sunboxnet@gmail.com>
2017-06-10 16:28:16 -0400

    Update config template and setup command to clone.

--- setup.rb
+++ setup.rb
@@ -9,7 +9,7 @@ YAML.load_file('config.yaml').each do |category, items|
   case category
   when  'repos'
     items.each do |item|
-      %x( #{SVN_CMDS[item[:type].to_sym][:command]} clone #{item[:url]} #{item[:destination]} )
+      %x( #{SVN_CMDS[item[:type].to_sym][:command]} clone https://#{item[:user]}:#{item[:pass]}@#{item[:domain]}/#{item[:user]}/#{item[:name]}.#{item[:type]} #{item[:destination]} )
     end
   else
     puts "don't know how to handle #{category}"
