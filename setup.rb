78b39a5568489e974eec451d30b8197ec704e0f0
Adam Lind <sunboxnet@gmail.com>
2017-06-11 02:02:16 -0400

    use strings instead of symbols to reference YAML config.

--- setup.rb
+++ setup.rb
@@ -9,7 +9,7 @@ YAML.load_file('config.yaml').each do |category, items|
   case category
   when  'repos'
     items.each do |item|
-      %x( #{SVN_CMDS[item[:type].to_sym][:command]} clone https://#{item[:user]}:#{item[:pass]}@#{item[:domain]}/#{item[:user]}/#{item[:name]}.#{item[:type]} #{item[:destination]} )
+      %x( #{SVN_CMDS[item['type'].try(:to_sym)]} clone https://#{item['user']}:#{item['pass']}@#{item['domain']}/#{item['user']}/#{item['name']}.#{item['type']} #{item['destination']} )
     end
   else
     puts "don't know how to handle #{category}"
