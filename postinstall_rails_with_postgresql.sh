c8f3a8ce87abd6492586857134b753d2e6947d09
Adam Lind <sunboxnet@gmail.com>
2018-02-03 23:37:19 -0500

    Do all the gem stuff at once.

--- postinstall_rails_with_postgresql.sh
+++ postinstall_rails_with_postgresql.sh
@@ -1,7 +1,4 @@
-rvm gemset create $1
-rvm gemset use $1
-gem install bundler
-bundle install
+rvm gemset create $1 && rvm gemset use $1 && gem install bundler && bundle install
 sudo apt-get install postgresql postgresql-client
 sudo su - postgres
 echo $2 | createuser --createdb --encrypted --login
